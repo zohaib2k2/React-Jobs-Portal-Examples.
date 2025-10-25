df1112fc1cd2276958787ca5c6c737617a344218
zohaib2k2 <zohaib.zafar2k2@gmail.com>
2025-10-26 01:12:07 +0500

    Created a ViewAllJobs component a page that would link to view all jobs

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,22 @@ import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import HomeCards from './components/HomeCards.jsx'
 import JobListings from './components/JobListings.jsx'
+import ViewAllJobs from './components/ViewAllJobs.jsx'
 
 const App = () => {
   return (
     <>
-    <Navbar />
+      <Navbar />
     
 
-    <Hero title="Become a React developer" subtitle="Find a suitable job according to your skills."/>
+      <Hero title="Become a React developer" subtitle="Find a suitable job according to your skills."/>
 
-    <HomeCards/>
+     <HomeCards/>
    
 
-    <JobListings/>    
+      <JobListings/>    
 
-    <section className="m-auto max-w-lg my-10 px-6">
-      <a
-        href="jobs.html"
-        className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >view all jobs</a
-      >
-    </section>
+      <ViewAllJobs/>
     </>
   )
 }
--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -3,9 +3,8 @@ import jobs from '../jobs.json';
 import JobListing from './jobListing';
 
 const JobListings = () => {
-    console.log(jobs); 
-    console.log(jobs[0].title);
-
+  
+  const recentJobs = jobs.slice(0,3);
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -14,7 +13,7 @@ const JobListings = () => {
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {/* <!-- job listing 1 --> */}
-          { jobs.map( (job) => (
+          { recentJobs.map( (job) => (
             <JobListing key={job.id} job={job}/>
           ))
           }
