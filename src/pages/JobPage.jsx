78b297b35ca2b6f92e1428ead828ba442e518c4d
zohaib2k2 <zohaib.zafar2k2@gmail.com>
2025-11-13 17:56:22 +0500

    Made it better Addjob with jobloader.

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -105,7 +105,7 @@ const JobPage = () => {
             <div className="bg-white p-6 rounded-lg shadow-md mt-6">
               <h3 className="text-xl font-bold mb-6">Manage Job</h3>
               <Link
-                to="/Addjob"
+                to={`/job/edit/${job.id}`}
                 class="bg-indigo-500 hover:bg-indigo-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
                 >Edit Job</Link
               >
@@ -122,7 +122,7 @@ const JobPage = () => {
       </div>
     </section>
 
-        </>)
+    </>)
 };
 
 
